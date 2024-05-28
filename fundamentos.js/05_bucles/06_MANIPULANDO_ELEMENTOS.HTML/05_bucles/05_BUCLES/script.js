173c28ccf8750a5197d1b9226e15a85f158cdb2c
matii777 <matiastoledo@liceovvh.cl>
2024-05-27 20:17:12 -0400

    ok

--- fundamentos.js/05_bucles/06_MANIPULANDO_ELEMENTOS.HTML/05_bucles/05_BUCLES/script.js
+++
@@ -1,9 +0,0 @@
-function traerTablas(){
-    let num = parseInt(document.getElementById("inputText").value);
-    //alert (num); 
-    for(let i = 1; i <= 10;i++){
-        result = `${i} X ${num} = ${i*num}<br>`
-    
-    }
-    document.getElementById("lista").innerHTML = result;
-}
